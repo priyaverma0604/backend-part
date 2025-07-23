f6980fd73396cf9f0606db078c32733b991adf68
priyaverma0604 <144715182+priyaverma0604@users.noreply.github.com>
2025-07-23 16:55:25 +0530

    cors updated

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
-const sellerRoutes = require('./routes/sellerRoutes');
-const buyerRoutes = require('./routes/buyerRoutes');
-const adminRoutes = require('./routes/adminRoutes');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const sellerRoutes = require("./routes/sellerRoutes");
+const buyerRoutes = require("./routes/buyerRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
 dotenv.config();
 const app = express();
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  cors({
+    origin: "https://cerulean-babka-e93fdd.netlify.app",
+    credentials: true,
+  })
+);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
-app.use('/api/auth', authRoutes);
-app.use('/api/seller', sellerRoutes);
-app.use('/api/buyer', buyerRoutes);
-app.use('/api/admin', adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/seller", sellerRoutes);
+app.use("/api/buyer", buyerRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.listen(5000, () => {
-  console.log('Server running on port 5000');
+  console.log("Server running on port 5000");
 });
